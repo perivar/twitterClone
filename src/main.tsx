c30545cab1116a2ef4dfd81414634c8fbb729fde
Per Ivar Nerseth <perivar@nerseth.com>
2021-11-29 20:15:28 +0100

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import {
+  Provider as PaperProvider,
+  DefaultTheme,
+  DarkTheme,
+} from 'react-native-paper';
+import { I18nManager } from 'react-native';
+import * as Updates from 'expo-updates';
+import { Appearance } from 'react-native';
+
+import { RootNavigator } from './rootNavigator';
+import { PreferencesContext } from './context/preferencesContext';
+
+export const Main = () => {
+  const colorScheme = Appearance.getColorScheme();
+  const [theme, setTheme] = React.useState<'light' | 'dark'>(
+    colorScheme === 'dark' ? 'dark' : 'light'
+  );
+  const [rtl] = React.useState<boolean>(I18nManager.isRTL);
+
+  function toggleTheme() {
+    setTheme(theme => (theme === 'light' ? 'dark' : 'light'));
+  }
+
+  const toggleRTL = React.useCallback(() => {
+    I18nManager.forceRTL(!rtl);
+    Updates.reloadAsync();
+  }, [rtl]);
+
+  const preferences = React.useMemo(
+    () => ({
+      toggleTheme,
+      toggleRTL,
+      theme,
+      rtl: (rtl ? 'right' : 'left') as 'right' | 'left',
+    }),
+    [rtl, theme, toggleRTL]
+  );
+
+  return (
+    <PreferencesContext.Provider value={preferences}>
+      <PaperProvider
+        theme={
+          theme === 'light'
+            ? {
+                ...DefaultTheme,
+                colors: { ...DefaultTheme.colors, primary: '#1ba1f2' },
+              }
+            : {
+                ...DarkTheme,
+                colors: { ...DarkTheme.colors, primary: '#1ba1f2' },
+              }
+        }
+      >
+        <RootNavigator />
+      </PaperProvider>
+    </PreferencesContext.Provider>
+  );
+};
